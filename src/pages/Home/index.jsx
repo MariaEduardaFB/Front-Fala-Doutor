cb84a797e25797f3c08cc6b4401b673cecebbd46
Maria Eduarda <ferreirabatistam34@gmail.com>
2025-11-21 18:20:14 -0300

    feat: installing styled components

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './style.css'
+
+function Home() {
+  
+
+  return (
+    <>
+      <div> 
+        <h1 className='titulo'>Fala Doutor</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default Home
